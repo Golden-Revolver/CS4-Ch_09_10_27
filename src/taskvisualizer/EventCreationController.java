ffcac39812bf3e2e6ac90b560a0e411403bb92f3
Kiley <Kiley@DESKTOP-KRRR9JL>
2024-03-15 16:05:28 +0800

    Event Creation Screen - ComboBox Entries

--- src/taskvisualizer/EventCreationController.java
+++ src/taskvisualizer/EventCreationController.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.beans.binding.*;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -29,9 +30,26 @@ import javafx.scene.text.*;
  * @author Kiley
  */
 public class EventCreationController extends UniversalController implements Initializable{
-
+    @FXML private ImageView headerIcon, resetIcon, addIcon;
+    @FXML private Button reset;
+    @FXML private Button add;
+    @FXML private TextField nameInput;
+    @FXML private TextField startHourInput, startMinuteInput;
+    @FXML private TextField endHourInput, endMinuteInput;
+    @FXML private DatePicker startDate, endDate;
+    @FXML private ComboBox<String> categorySelector;
+    @FXML private ComboBox<String> startDayNightPicker;
+    @FXML private ComboBox<String> endDayNightPicker;
+    private Image headerImage, resetImage, addImage;
+    private ArrayList<String> DayNight = new ArrayList<String>();
+    
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        DayNight.add("AM");
+        DayNight.add("PM");
+        endDayNightPicker.setItems(FXCollections.observableArrayList(DayNight));
+        startDayNightPicker.setItems(FXCollections.observableArrayList(DayNight));
         
     }
 }
