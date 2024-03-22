919d17e5506cd73a7042378ea4ce900a0b2cffdc
Christian Brandon <Christian Brandon@LAPTOP-LM26I41K>
2024-03-22 23:38:21 +0800

    Add model classes and the ability to select days in the calendar

---
+++ src/taskvisualizer/Goal.java
@@ -0,0 +1,137 @@
+package taskvisualizer;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+/**
+ *
+ * @author Christian Brandon
+ */
+public class Goal extends Task implements Comparable<Goal> {
+    private LocalDate deadline;
+    private double progress, target;
+    private final double initial;
+    private boolean reward, late;
+    
+    public Goal(String name, double target, LocalDate deadline) {
+        super(name);
+        this.target = target;
+        progress = 0;
+        reward = true;
+        this.deadline = deadline;
+        initial = progress;
+        late = LocalDate.now().isAfter(deadline);
+
+    }
+    public Goal(String name, double target, 
+            boolean reward, LocalDate deadline) {
+        super(name);
+        this.target = target;
+        progress = 0;
+        initial = progress;
+        this.reward = reward;
+        this.deadline = deadline;
+        late = LocalDate.now().isAfter(deadline);
+    }
+    public Goal(String name, double progress, 
+            double target, LocalDate deadline) {
+        super(name);
+        this.progress = progress;
+        initial = progress;
+        this.target = target;
+        reward = true;
+        this.deadline = deadline;
+        late = LocalDate.now().isAfter(deadline);
+    }
+    public Goal(String name, double progress, double target, 
+            boolean reward, LocalDate deadline) {
+        super(name);
+        this.progress = progress;
+        initial = progress;
+        this.target = target;
+        this.reward = reward;
+        this.deadline = deadline;
+        late = LocalDate.now().isAfter(deadline);
+    }
+    
+    @Override
+    public int compareTo(Goal g) {
+        if (deadline.equals(g.getDeadline())) return 0;
+        else if (deadline.isAfter(g.getDeadline())) return 1;
+        else return -1;
+    }
+    
+    public double getProgress() {
+        return progress;
+    }
+    public void setProgress(double progress) {
+        this.progress = progress;
+        updateGoal();
+    }
+    public void updateProgress(double change) {
+        progress += change;
+        updateGoal();
+    }
+    
+    public double getTarget() {
+        return target;
+    }
+    public void setTarget(double target) {
+        this.target = target;
+    }
+    
+    public double getInitial() {
+        return initial;
+    }
+    
+    public boolean getReward() {
+        return reward;
+    }
+    public void setReward(boolean reward) {
+        this.reward = reward;
+    }
+    
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+    
+    public boolean isLate() {
+        return late;
+    }
+    public void setLate(boolean late) {
+        this.late = late;
+    }
+    public void updateLate() {
+        late = LocalDate.now().isAfter(deadline);
+    }
+    
+    @Override
+    public String getNameFormat() {
+        return name;
+    }
+    @Override
+    public String getDataFormat() {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+        return String.format("Progress: %.0f/%.0f%nFinish by %s",
+                progress, target, deadline.format(dateFormat));
+    }
+    
+    public double getGoalFraction() {
+        double goalFraction;
+        if (reward) goalFraction = progress / target;
+        else goalFraction = (initial - progress) / (initial - target);
+        
+        // must be a value between 0 and 1
+        if (goalFraction < 0) goalFraction = 0;
+        if (goalFraction > 1) goalFraction = 1;
+        return goalFraction;
+    }
+    
+    /* If reward is true, the User is rewarded for increasing progress.
+    If reward is false, the User is rewarded for decreasing progress. */
+    public void updateGoal() {
+        int counter = reward ? 1 : -1;
+        complete = (progress * counter >= target * counter);
+    }
+}
--- src/taskvisualizer/TaskVisualizer.java
+++ src/taskvisualizer/TaskVisualizer.java
@@ -1,5 +1,8 @@
 package taskvisualizer;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -28,6 +31,56 @@ public class TaskVisualizer extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        User u1 = new User("Golden Revolver", "GoldenMan1234");
+        UniversalController.setUser(u1);
+        /* This is temporary. In the final app, Users will be created from the sign-up
+        screen and added to the userList. Upon log-in, findUser() will take the name
+        and password as parameters. If there is a match, the returned User will be
+        set as the currentUser with setUser().
+        */
+        
+        // Scenario 1: School Day
+        LocalDateTime d1 = LocalDateTime.of(2024, 1, 22, 0, 0);
+        LocalDateTime d2 = LocalDateTime.of(2024, 1, 16, 18, 0);
+        LocalDateTime d3 = LocalDateTime.of(2024, 2, 12, 15, 22);
+        Requirement r1 = new Requirement("Literature Review", "Research", d1);
+        Requirement r2 = new Requirement("Family Pedigree", "Biology", d2);
+        Requirement r3 = new Requirement("Exercise 12", "Computer Science", d3);
+        
+        u1.addTask(r1);
+        u1.addTask(r2);
+        u1.addTask(r3);
+        
+        // Scenario 2: Family Reunion
+        LocalDateTime d4 = LocalDateTime.of(2024, 6, 30, 12, 30);
+        LocalDateTime d5 = LocalDateTime.of(2024, 7, 24, 8, 0);
+        Event e1 = new Event("Family Gathering", d4, "Sorsogon");
+        Event e2 = new Event("School Fair", d5, "PSHS-MC");
+        
+        u1.addTask(e1);
+        u1.addTask(e2);
+        
+        // Scenario 3: Workout Plan
+        LocalDate d6 = LocalDate.of(2024, 1, 30);
+        LocalDate d7 = LocalDate.of(2024, 2, 12);
+        Habit h1 = new Habit("Complete exercise plan");
+        Goal g1 = new Goal("Push-ups", 10, d6);
+        Goal g2 = new Goal("Lose weight", 50.0, 40.0, false, d7);
+        
+        u1.addTask(h1);
+        u1.addTask(g1);
+        u1.addTask(g2);
+        
+        // Testing
+        LocalDateTime d8 = LocalDateTime.of(2024, 4, 1, 0, 0);
+        Event e3 = new Event("Cat birthday :3", d8);
+        Event e4 = new Event("April Fools", d8);
+        Requirement r4 = new Requirement("Protocol Listing", "Research", d8);
+        
+        u1.addTask(e3);
+        u1.addTask(e4);
+        u1.addTask(r4);
+        
         launch(args);
     }
 }
